d73bb3c0ed84a13164f45caf714baf916cffa175
Antonio Ortiz Avila <antonio.ortiz.avila@gmail.com>
2025-11-08 10:37:55 -0600

    Change npm to pnpm like as packages manager. Fix files .lintstagedrc.json and .husky/pre-commit.

--- src/app/features/auth/components/forgot-password/forgot-password.component.ts
+++ src/app/features/auth/components/forgot-password/forgot-password.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -17,7 +17,6 @@ import { CustomValidators } from '../../../../shared/validators/custom-validator
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterLink,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
